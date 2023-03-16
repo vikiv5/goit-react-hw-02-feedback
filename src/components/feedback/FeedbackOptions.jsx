6baf9cc66e38d78dfdd55d02c37e5993b0d07dfc
vikiv5 <vi_kov@ukr.net>
2023-03-16 23:37:31 +0200

    Update FeedbackOptions.jsx

--- src/components/feedback/FeedbackOptions.jsx
+++ src/components/feedback/FeedbackOptions.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types"
 import { ButtonDiv , Button } from "./Feedback.styled";
-//import React from "react";
+
 
 export const FeedbackOptions = ({options , handleClickBtn}) =>{
     return (
