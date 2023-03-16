f9e6855df515f113f052ffaa1932874aa1801623
vikiv5 <vi_kov@ukr.net>
2023-03-17 00:30:28 +0200

    Update Statistics.styled.js

--- src/components/feedback/Statistics.styled.js
+++ src/components/feedback/Statistics.styled.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
         list-style: none;
         margin: 0;
         padding-left: 0;
-        font-family: 'Roboto Slab', sans-serif;
         font-size: 20px;
         font-weight: 500;
         & li{
@@ -19,25 +18,5 @@ import styled from "styled-components";
     `;
 
     export const Value = styled.span`
-        color: ${props => {
-            switch(props.name) {
-            case 'good':
-            return '#32CD32';
-            case 'neutral':
-            return '#D2691E';
-            case 'bad':
-            return '#FF0000';
-            case 'total':
-            return '#00008B';
-            case 'positivePercentage':
-                if(parseInt(props.children) >= 75) return '#006400';
-                if(parseInt(props.children) < 75 && parseInt(props.children) >= 50  ) return '#D2691E';
-                if(parseInt(props.children) < 50) return '#FF0000';
-                break;
-            
-            default:
-            return '#6495ED'
-            }
-        }};
-          
+        color: blue
     `;
